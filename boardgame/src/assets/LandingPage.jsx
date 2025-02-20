336964928078283a497984c606060b303fdbda4e
maticdremelj <matic.dremel@gmail.com>
2025-02-20 19:16:50 +0100

    Link nested routes

--- boardgame/src/assets/LandingPage.jsx
+++ boardgame/src/assets/LandingPage.jsx
@@ -1,7 +1,21 @@
-const LandingPage = () => (
-    <div>
-      Landing Page
+import { useNavigate } from 'react-router-dom';
+
+const LandingPage = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex items-start justify-center  h-screen">
+      <div className="mt-[33vh] text-center">
+        <h1>Landing Page</h1>
+        <button
+          onClick={() => navigate('/lobby')}
+          className="border-2"
+        >
+          Go to Lobby
+        </button>
+      </div>
     </div>
   );
+};
 
 export default LandingPage;
--- boardgame/src/assets/Lobby.jsx
+++ boardgame/src/assets/Lobby.jsx
@@ -1,10 +1,31 @@
 import { Outlet } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
-const Lobby = () => (
-    <div>
-      Lobby
-      <Outlet />
+const Lobby = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex items-start justify-center  h-screen">
+      <div className="mt-[33vh] text-center">
+        <h1>Lobby</h1>
+        <div className="flex flex-col">
+          <button
+            onClick={() => navigate('join')}
+            className="border-2"
+          >
+            Join
+          </button>
+          <button
+            onClick={() => navigate('host')}
+            className="border-2"
+          >
+            Host
+          </button>
+        </div>
+        <Outlet />
+      </div>
     </div>
   );
+};
 
 export default Lobby;
