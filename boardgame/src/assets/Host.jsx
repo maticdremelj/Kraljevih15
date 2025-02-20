1c97d81f6ed0012166676562f147e81fa6f8b210
maticdremelj <matic.dremel@gmail.com>
2025-02-21 00:46:57 +0100

    Initialize firestore

--- boardgame/src/assets/Host.jsx
+++ boardgame/src/assets/Host.jsx
@@ -1,7 +1,13 @@
-const Host = () => (
-  <div>
-    Host
-  </div>
-);
+import { collection, addDoc } from 'firebase/firestore';
+import { db } from '../firebaseConfig';
+
+const Host = () => {
+  
+  return (
+    <div>
+      Host
+    </div>
+  );
+};
 
 export default Host;
