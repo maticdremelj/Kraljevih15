259bc584be936606f20e153146f36dd10aeed56f
maticdremelj <matic.dremel@gmail.com>
2025-02-20 15:09:30 +0100

    Host on firebase

--- boardgame/src/App.jsx
+++ boardgame/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import RoutesComponents from './assets/RoutesComponents';
 
 const App = () => (
-  <Router basename="/board-game">
+  <Router>
     <RoutesComponents />
   </Router>
 );
