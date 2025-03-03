d7b24d176e45ec298fd4cc5b2b0e31699dd53bcb
maticdremelj <matic.dremel@gmail.com>
2025-03-03 18:21:25 +0100

    Lazy loading

--- boardgame/src/assets/RoutesComponents.jsx
+++ boardgame/src/assets/RoutesComponents.jsx
@@ -1,17 +1,33 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { lazy, Suspense } from "react";
 import Board from './Board';
 import Lobby from './Lobby';
 import LandingPage from './LandingPage';
-import Host from './Host';
-import Join from './Join';
 import NotFound from './NotFound';
+
+const Host = lazy(() => import("./Host"));
+const Join = lazy(() => import("./Join"));
 
 const RoutesComponents = () => (
   <Routes>
     <Route path="/" element={<LandingPage />} />
 	  <Route path="/lobby" element={<Lobby />}>
-      <Route path="host" element={<Host />} />
-      <Route path="join" element={<Join />} />
+      <Route 
+        path="host" 
+        element={
+          <Suspense>
+            <Host />
+          </Suspense>
+        } 
+      />
+      <Route
+        path="join"
+        element={
+          <Suspense>
+            <Join />
+          </Suspense>
+        }
+      />
     </Route>
     <Route path="*" element={<NotFound />} />
   </Routes>
