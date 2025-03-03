bf9062f7425b709518f0703d2a385ad78aeb2e04
maticdremelj <matic.dremel@gmail.com>
2025-03-03 17:06:53 +0100

    Rollup plugin visualizer integration

--- boardgame/vite.config.js
+++ boardgame/vite.config.js
@@ -1,6 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    visualizer({
+      filename: 'dist/stats.html', 
+      template: 'sunburst', // 'sunburst', 'treemap', 'network'
+      open: true,
+      gzipSize: true,
+      brotliSize: true 
+    })
+  ]
 })
